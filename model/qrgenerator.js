353e46a19d078413c3e87523fb3e1b10ec455f5b
GeremiaPompei <56674278+GeremiaPompei@users.noreply.github.com>
2020-12-05 11:25:09 +0100

    Update qrgenerator.js

--- model/qrgenerator.js
+++ model/qrgenerator.js
@@ -4,7 +4,7 @@ const qrcode = require('qrcode');
 
 async function generate(path, name) {
     var finalpath = path+'.png';
-    var strGenerated = process.env.URL+'/pdfqueue/'+name;
+    var strGenerated = process.env.URL+'pdfqueue/'+name;
     await qrcode.toFile(finalpath, strGenerated);
     console.log('Genarated: '+strGenerated);
     return finalpath;
