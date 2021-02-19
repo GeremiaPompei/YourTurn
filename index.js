12c1035d9245e15f4ed5ad0cd43c7af48d7a6771
Geremia <geremia.pompei@studenti.unicam.it>
2021-02-19 13:15:08 +0100

    update

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const app = express();
 const port = process.env.PORT||3000;
 
 app.use(route);
+app.use('/', express.static(__dirname + '/client'));
 
 app.listen(port,()=>console.log('You are connected: '+port));
